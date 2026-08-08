722289b71250a5a31288f04fd253fd14e0c833b4
BeleamBlake <beleamblake@gmail.com>
2026-08-08 01:31:23 -0600

    validaciones Regex y registro, sin fallas

--- app/src/main/java/com/example/appgym/SingletonData.kt
+++ app/src/main/java/com/example/appgym/SingletonData.kt
@@ -13,19 +13,21 @@ object SingletonData {
         arrayList_mensajes.add(0, "Los campos no pueden estar vacios")
         arrayList_mensajes.add(1, "El formato del correo no es valido")
         arrayList_mensajes.add(2, "Solo usar numero para la contraseña")
-        arrayList_mensajes.add(3, "Caracter invalido para el nombre")
+        arrayList_mensajes.add(3, "Nombre invalido")
         arrayList_mensajes.add(4, "El numero de celular invalido")
-        arrayList_mensajes.add(5, "Apellido invalido, usar solo letras")
+        arrayList_mensajes.add(5, "Apellido invalido")
         arrayList_mensajes.add(6, "No ingrese espacios")
         arrayList_mensajes.add(7, "No se pudo registrar al nuevo miembro")
         arrayList_mensajes.add(8, "Bienvenido a ExoFit Elite")
-        arrayList_mensajes.add(9, "No se registro en la base de datos")
+        arrayList_mensajes.add(9, "Miembro registrado con exito")
+        arrayList_mensajes.add(10, "No se registro en la base de datos")
+
 
 
         arrayList_validaciones = ArrayList<String>()
         arrayList_validaciones.add(0, "^[A-Za-z+_.-]+@[A-Za-z0-9.-]+$") // correo electronico
-        arrayList_validaciones.add(1, "[0-9]")// validador de contraseña y numero de celular
-        arrayList_validaciones.add(3, "[A-Za-záeéiíoóuúüñAÁEÉIÍOÓUÚÜÑ]")//validador de nombre y apellido
+        arrayList_validaciones.add(1, "[0-9]{10}")// validador de contraseña y numero de celular
+        arrayList_validaciones.add(3, "[A-Za-záeéiíoóuúüñAÁEÉIÍOÓUÚÜÑ]+$")//validador de nombre y apellido
 
 
 
