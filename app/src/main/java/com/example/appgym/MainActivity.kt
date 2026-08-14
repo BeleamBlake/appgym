679b22d7882a9cd2d3e055f568301c8d8e37aade
gatolico <skidsam2@gmail.com>
2026-08-14 00:23:38 -0600

    14/08/26

--- app/src/main/java/com/example/appgym/MainActivity.kt
+++ app/src/main/java/com/example/appgym/MainActivity.kt
@@ -61,11 +61,11 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        val boton_registro = findViewById<Button>(R.id.bt_registro)
-        boton_registro.setOnClickListener {
-            val intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
-        }
+//        val boton_registro = findViewById<Button>(R.id.bt_registro)
+//        boton_registro.setOnClickListener {
+//            val intent = Intent(this, MainActivity3::class.java)
+//            startActivity(intent)
+//        }
 
         val btRecuperar = findViewById<Button>(R.id.bt_recuperar)
         btRecuperar.setOnClickListener {
--- app/src/main/java/com/example/appgym/Progreso.kt
+++ app/src/main/java/com/example/appgym/Progreso.kt
@@ -1,28 +1,24 @@
 package com.example.appgym
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Progreso.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Progreso : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -30,25 +26,55 @@ class Progreso : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_progreso, container, false)
+
+        return inflater.inflate(
+            R.layout.fragment_progreso,
+            container,
+            false
+        )
+    }
+
+    // AQUÍ DETECTAMOS EL CLICK
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val boton = view.findViewById<TextView>(
+            R.id.btn_registrar_entrada
+        )
+
+        boton.setOnClickListener {
+            mostrarFormulario()
+        }
+    }
+
+    private fun mostrarFormulario() {
+
+        val vista = layoutInflater.inflate(
+            R.layout.registrar_entrada,
+            null
+        )
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(vista)
+            .create()
+
+        dialog.show()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Progreso.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(
+            param1: String,
+            param2: String
+        ) =
             Progreso().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
